17fb198d482f53242a51b9666236a505c7a091b0
shin-ito0306 <shin.20000306@icloud.com>
2021-08-25 06:59:02 +0000

    [finish]part5

---
+++ Chapter05.java
@@ -0,0 +1,21 @@
+public class Chapter05 {
+  public static void main(String[] args) {
+    long long_num = 10L;
+    System.out.println(long_num);
+    double double_num = 10d;
+    System.out.println(double_num);
+    boolean boolean1 = true;
+    System.out.println(boolean1);
+    
+    Float float_num = 0.1f;
+    System.out.println(float_num);
+    Integer integer_num = 2;
+    System.out.println(integer_num);
+    Character chara = 'あ';
+    System.out.println(chara);
+    
+    int[] array = new int[5];
+    array[3] = 3;
+    System.out.println(array[3]);
+  }
+}
