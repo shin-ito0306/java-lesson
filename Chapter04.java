3fa67363cc8a3be268a1c9e170d777afc91d3f56
shin-ito0306 <shin.20000306@icloud.com>
2021-08-25 06:29:25 +0000

    [finish]part4

---
+++ Chapter04.java
@@ -0,0 +1,12 @@
+public class Chapter04 {
+  public static void main(String[] args) {
+    String name = "伊藤辰";
+    System.out.println(name);
+    
+    name = "ありがとう";
+    System.out.println(name);
+    
+    final int NUM = 1;
+    System.out.println(NUM);
+  }
+}
